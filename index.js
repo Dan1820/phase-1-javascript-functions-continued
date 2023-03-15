e464dcc2d214589c6ef969c6d07f3d9c3d96c708
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-15 17:58:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+
+
+function saturdayFun(activity='roller-skate'){
+    
+    
+    
+
+        // return `This Saturday, I want to ${activity}! `
+        return `This Saturday, I want to ${activity}!`
+        // console.log(`This Saturday, I want to ${activity} `)
+  
+}
+// console.log(saturdayFun('Drink'));
+console.log(saturdayFun());
+
+function mondayWork(workDone='go to the office'){
+    // return `This Monday, ${workDone}.`
+    return `This Monday, I will ${workDone}.`
+
+}
+// console.log(mondayWork())
+// console.log(mondayWork(`This Monday, I will work from home`))
+
+function wrapAdjective(adj='*'){
+    // let result = wrapAdjective('*')
+      return function(action='a hard worker'){
+
+      
+      return `You are ${adj}${action}${adj}!`
+      }
+
+}
